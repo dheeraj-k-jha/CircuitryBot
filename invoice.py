1a37a9c850534ffb090dc68c6ca04a49c2f39f61
Dheeraj jha <justdheeit101@gmail.com>
2026-07-10 01:14:28 +0530

    fixed invoice wrap

--- invoice.py
+++ invoice.py
@@ -15,19 +15,40 @@ def generate_invoice_pdf(order_id):
     pdf.cell(0, 8, f"Date: {order['date']}", ln=True, align="C")
     pdf.ln(10)
 
+#-------------------------Items--------------------------------------------------
     pdf.set_font("Arial", "B", 11)
-    pdf.cell(100, 8, "Item", border=1)
+    pdf.cell(90, 8, "Item", border=1)
     pdf.cell(20, 8, "Qty", border=1, align="C")
     pdf.cell(35, 8, "Price", border=1, align="R")
     pdf.cell(35, 8, "Subtotal", border=1, align="R", ln=True)
 
-    pdf.set_font("Arial", "", 11)
-    for item in order["items"]:
-        pdf.cell(100, 8, item["name"], border=1)
-        pdf.cell(20, 8, str(item["qty"]), border=1, align="C")
-        pdf.cell(35, 8, f"Rs.{item['price']}", border=1, align="R")
-        pdf.cell(35, 8, f"Rs.{item['qty'] * item['price']}", border=1, align="R", ln=True)
+    pdf.set_font("Arial", "", 10)
+    row_height = 6
 
+    for item in order["items"]:
+        item_name = item["name"]
+        qty = item["qty"]
+        price = item["price"]
+        subtotal = qty * price
+
+        # how many lines will the item name take at this column width?
+        lines_needed = pdf.multi_cell(90, row_height, item_name, border=0, split_only=True)
+        num_lines = len(lines_needed)
+        cell_h = row_height * num_lines
+
+        x_start = pdf.get_x()
+        y_start = pdf.get_y()
+
+        # draw the wrapped item name
+        pdf.multi_cell(90, row_height, item_name, border=1)
+
+        # move cursor back up to draw the other 3 columns at matching height
+        pdf.set_xy(x_start + 90, y_start)
+        pdf.cell(20, cell_h, str(qty), border=1, align="C")
+        pdf.cell(35, cell_h, f"Rs.{price}", border=1, align="R")
+        pdf.cell(35, cell_h, f"Rs.{subtotal}", border=1, align="R", ln=True)
+
+    #--------------------------Total------------------------    
     pdf.ln(5)
     pdf.set_font("Arial", "B", 12)
     pdf.cell(0, 10, f"Total: Rs.{order['total']}", ln=True, align="R")
